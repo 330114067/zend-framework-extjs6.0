2c627c67658e0b520b5d57b8b688f81196691a2b
330114067 <330114067@163.com>
2017-12-28 16:55:06 +0800

    赵天鑫 2017-12-29 12:20 增加doc目录

---
+++ config/autoload/local.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'db' => array(
+        'username' => 'root',
+        'password' => 'root',
+    ),
+    'session' => array(
+        'config' => array(
+            'class' => 'Zend\Session\Config\SessionConfig',
+            'options' => array(
+                'name' => 'Admin',
+            ),
+        ),
+        'storage' => 'Zend\Session\Storage\SessionArrayStorage',
+        'validators' => array(
+            'Zend\Session\Validator\RemoteAddr',
+            'Zend\Session\Validator\HttpUserAgent',
+        ),
+    ),
+);
