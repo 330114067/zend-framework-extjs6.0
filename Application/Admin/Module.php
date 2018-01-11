32deaa8cd5127b784e17d029ab37c46520b301e9
330114067 <330114067@163.com>
2018-01-11 15:08:29 +0800

    代码格式化排版

--- Application/Admin/Module.php
+++ Application/Admin/Module.php
@@ -94,79 +94,75 @@ class Module
             ),
         );
     }
-    // Add this method:
+    
     public function getServiceConfig()
     {
         return array(
             'factories' => array(
                 'Admin\Model\userTable' =>  function($sm) {
-                $tableGateway = $sm->get('UserTableGateway');
-                $table = new userTable($tableGateway);
-                return $table;
+	                $tableGateway = $sm->get('UserTableGateway');
+	                $table = new userTable($tableGateway);
+	                return $table;
                 },
                 'UserTableGateway' => function ($sm) {
-                $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                $resultSetPrototype = new ResultSet();
-                $resultSetPrototype->setArrayObjectPrototype(new user());
-                return new TableGateway('t_user', $dbAdapter, null, $resultSetPrototype);
+                	$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                	$resultSetPrototype = new ResultSet();
+               		$resultSetPrototype->setArrayObjectPrototype(new user());
+                	return new TableGateway('t_user', $dbAdapter, null, $resultSetPrototype);
                 },
                 'Admin\Model\fidTable' =>  function($sm) {
-                $tableGateway = $sm->get('FidTableGateway');
-                $table = new fidTable($tableGateway);
-                return $table;
+                	$tableGateway = $sm->get('FidTableGateway');
+                	$table = new fidTable($tableGateway);
+                	return $table;
                 },
                 'FidTableGateway' => function ($sm) {
-                $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                $resultSetPrototype = new ResultSet();
-                $resultSetPrototype->setArrayObjectPrototype(new fid());
-                return new TableGateway('t_fid', $dbAdapter, null, $resultSetPrototype);
+                	$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                	$resultSetPrototype = new ResultSet();
+                	$resultSetPrototype->setArrayObjectPrototype(new fid());
+                	return new TableGateway('t_fid', $dbAdapter, null, $resultSetPrototype);
                 },
                 'Zend\Session\SessionManager' => function ($sm) {
-                $config = $sm->get('config');
-                if (isset($config['session'])) {
-                    $session = $config['session'];
-                    
-                    $sessionConfig = null;
-                    if (isset($session['config'])) {
-                        $class = isset($session['config']['class'])  ? $session['config']['class'] : 'Zend\Session\Config\SessionConfig';
-                        $options = isset($session['config']['options']) ? $session['config']['options'] : array();
-                        $sessionConfig = new $class();
-                        $sessionConfig->setOptions($options);
-                    }
-                    
-                    $sessionStorage = null;
-                    if (isset($session['storage'])) {
-                        $class = $session['storage'];
-                        $sessionStorage = new $class();
-                    }
-                    
-                    $sessionSaveHandler = null;
-                    if (isset($session['save_handler'])) {
-                        // class should be fetched from service manager since it will require constructor arguments
-                        $sessionSaveHandler = $sm->get($session['save_handler']);
-                    }
-                    
-                    $sessionManager = new SessionManager($sessionConfig, $sessionStorage, $sessionSaveHandler);
-                } else {
-                    $sessionManager = new SessionManager();
-                }
-                Container::setDefaultManager($sessionManager);
-                return $sessionManager;
-                },
-                ), 
-                );
+                	$config = $sm->get('config');
+	                if(isset($config['session'])) {
+	                    $session = $config['session'];
+	                    $sessionConfig = null;
+	                    if (isset($session['config'])) {
+	                        $class = isset($session['config']['class'])  ? $session['config']['class'] : 'Zend\Session\Config\SessionConfig';
+	                        $options = isset($session['config']['options']) ? $session['config']['options'] : array();
+	                        $sessionConfig = new $class();
+	                        $sessionConfig->setOptions($options);
+	                    }
+	                    
+	                    $sessionStorage = null;
+	                    if (isset($session['storage'])) {
+	                        $class = $session['storage'];
+	                        $sessionStorage = new $class();
+	                    }
+	                    
+	                    $sessionSaveHandler = null;
+	                    if (isset($session['save_handler'])) {
+	                        $sessionSaveHandler = $sm->get($session['save_handler']);
+	                    }
+	                    
+	                    $sessionManager = new SessionManager($sessionConfig, $sessionStorage, $sessionSaveHandler);
+	               }else{
+	                    $sessionManager = new SessionManager();
+	               }
+               		Container::setDefaultManager($sessionManager);
+               		return $sessionManager;
+               },
+        	), 
+    	);
     }
     public function registerJsonStrategy($e)
     {
         $matches    = $e->getRouteMatch();
 //         $moduleRootName = $matches->getMatchedRouteName();//$moduleaRootName是在module.config.php中配置的route名称
         
-        
         $app          = $e->getTarget();
         $locator      = $app->getServiceManager();
         $view         = $locator->get('Zend\View\View');
         $jsonStrategy = $locator->get('ViewJsonStrategy');
-        
         $view->getEventManager()->attach($jsonStrategy, 100);
     }  
 
