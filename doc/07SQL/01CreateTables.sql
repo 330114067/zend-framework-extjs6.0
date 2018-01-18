890fbcd045a5d4ea2c36241bdda6488be740ba0d
330114067 <330114067@163.com>
2018-01-18 13:53:50 +0800

    修改创建表的字段

--- doc/07SQL/01CreateTables.sql
+++ doc/07SQL/01CreateTables.sql
@@ -1,7 +1,3 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 DROP TABLE IF EXISTS `t_biz_log`;
 CREATE TABLE IF NOT EXISTS `t_biz_log` (
@@ -16,6 +12,7 @@ CREATE TABLE IF NOT EXISTS `t_biz_log` (
   `company_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 
 DROP TABLE IF EXISTS `t_fid`;
 CREATE TABLE IF NOT EXISTS `t_fid` (
@@ -35,26 +32,37 @@ CREATE TABLE IF NOT EXISTS `t_menu_item` (
 
 DROP TABLE IF EXISTS `t_org`;
 CREATE TABLE IF NOT EXISTS `t_org` (
-  `id` varchar(255) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `org_code` varchar(255) NOT NULL,
-  `parent_id` varchar(255) DEFAULT NULL,
-  `data_org` varchar(255) DEFAULT NULL,
+  `id` varchar(255) NOT NULL COMMENT '主键',
+  `full_name` varchar(255) NOT NULL COMMENT '组织机构全路径名称',
+  `name` varchar(255) NOT NULL COMMENT '组织机构名称',
+  `org_code` varchar(255) NOT NULL COMMENT '组织机构编码',
+  `parent_id` varchar(255) DEFAULT NULL COMMENT '上级组织机构的id',
+  `data_org` varchar(255) DEFAULT NULL COMMENT '数据域',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment'组织机构';
 
-DROP TABLE IF EXISTS `t_permission`;
-CREATE TABLE IF NOT EXISTS `t_permission` (
-  `id` varchar(255) NOT NULL,
-  `fid` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `note` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
-  `py` varchar(255) DEFAULT NULL,
-  `show_order` int(11) DEFAULT NULL,
+drop table if exists `t_navigation`;
+CREATE TABLE `t_navigation` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL COMMENT '菜单名称',
+  `keyid` int(4) DEFAULT '0' COMMENT '上级id',
+  `xtypeClass` varchar(70)  COMMENT '对应链接',
+  `menu` tinyint(1) DEFAULT NULL COMMENT '是否菜单显示',
+  `leaf` varchar(30) DEFAULT '0' COMMENT '是否是叶子节点  ',
+  `sort` smallint(2) DEFAULT '0' COMMENT '排序，越大排在越前',
+  `display` tinyint(1) DEFAULT NULL COMMENT '是否显示，0为显示，1为不显示',
+	`store` varchar(40) default null comment '加载数据地址',
+	`iconCls` varchar(30) default null comment'菜单左边图标',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 comment'权限';
+
+drop table if exists `t_user_group`;
+CREATE TABLE `t_user_group` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(20) NOT NULL comment'自增id',
+  `group_power` text NOT NULL comment'权限列表序列化格式',
+  PRIMARY KEY (`id`)
+) ENGINE=Innodb DEFAULT CHARSET=utf8 comment'角色';
 
 DROP TABLE IF EXISTS `t_recent_fid`;
 CREATE TABLE IF NOT EXISTS `t_recent_fid` (
@@ -63,37 +71,17 @@ CREATE TABLE IF NOT EXISTS `t_recent_fid` (
   `click_count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_role`;
-CREATE TABLE IF NOT EXISTS `t_role` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `data_org` varchar(255) DEFAULT NULL,
-  `company_id` varchar(255) DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_role_permission`;
-CREATE TABLE IF NOT EXISTS `t_role_permission` (
-  `role_id` varchar(255) DEFAULT NULL,
-  `permission_id` varchar(255) DEFAULT NULL,
-  `data_org` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `t_role_user`;
-CREATE TABLE IF NOT EXISTS `t_role_user` (
-  `role_id` varchar(255) DEFAULT NULL,
-  `user_id` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
-  `id` varchar(225) NOT NULL COMMENT '主键',
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键自增',
   `enabled` int(11) NOT NULL COMMENT '用户是否可以登录',
   `login_name` varchar(255) NOT NULL COMMENT '用户登录名',
-  `name` varchar(255) NOT NULL COMMENT '用户名称',
-  `org_id` varchar(255) NOT NULL COMMENT '组织机构id',
-  `org_code` varchar(255) NOT NULL COMMENT '用户的组织机构编码',
+  `name` varchar(255) DEFAULT NULL COMMENT '用户名称',
+  `group_id` int(10) DEFAULT NULL,
+  `org_id` int(10) NOT NULL COMMENT '组织机构id',
   `password` varchar(255) NOT NULL COMMENT '登录密码',
   `py` varchar(255) DEFAULT NULL COMMENT '用户名称的拼音字头',
   `gender` varchar(255) DEFAULT NULL COMMENT '性别',
@@ -103,7 +91,6 @@ CREATE TABLE `t_user` (
   `tel02` varchar(255) DEFAULT NULL COMMENT '备用联系电话',
   `address` varchar(255) DEFAULT NULL COMMENT '家庭住址',
   `data_org` varchar(255) DEFAULT NULL COMMENT '数据域',
-  `company_id` varchar(255) DEFAULT NULL COMMENT '公司id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统用户表';
 
